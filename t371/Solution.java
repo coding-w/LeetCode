388908e0bf359f9f4525266c7d96a1a46140fc85
town-coding <wangx.coding@gmail.com>
2025-01-07 09:25:19 +0800

    feat: 20250107

---
+++ t371/Solution.java
@@ -0,0 +1,16 @@
+package t371;
+
+/**
+ * @author wangxiang
+ * @description
+ * @create 2025/1/6 09:33
+ */
+class Solution {
+    public int getSum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
